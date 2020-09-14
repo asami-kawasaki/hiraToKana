e41d4dc6dc96c1fb5e18e00eee1a6010aad809ba
asami kawasaki <asami.kawasaki@blueish.co.jp>
2020-09-14 15:56:21 +0900

    テスト３

--- hira-to-kana/src/HiraToKana.js
+++ hira-to-kana/src/HiraToKana.js
@@ -2,13 +2,12 @@ import React, { useState } from 'react';
 import './App.css';
 
 function HiraToKana(props) {
-  let katakana = () => {
-    props.str.replace(/[\u30a1-\u30f6]/g,
+  let katakana = props.str.replace(/[\u30a1-\u30f6]/g,
       function (match) {
         var chr = match.charCodeAt(0) - 0x60;
         return String.fromCharCode(chr);
     });
+    return <div>{katakana}</div>;
 };
-return <div>{katakana}</div>;
 
 export default HiraToKana;
